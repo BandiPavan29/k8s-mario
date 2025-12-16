c50d7cf843adf7e28a4e0bfa1ee19ebdd729bddf
BandiPavan29 <bandipavan2904@gmail.com>
2025-12-16 22:27:03 +0530

    Change S3 bucket name from mario143 to mario142
    Updated the S3 bucket name for the Terraform backend.

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mario143" # Replace with your actual S3 bucket name
+    bucket = "mario142" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
