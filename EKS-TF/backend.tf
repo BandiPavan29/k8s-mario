3d5dcb579ff3efc338900adf71e46ce2635a2396
BandiPavan29 <bandipavan2904@gmail.com>
2025-12-22 11:41:21 +0530

    Change S3 bucket name in backend.tf
    Updated S3 bucket name for Terraform backend.

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mario142" # Replace with your actual S3 bucket name
+    bucket = "game1267" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
